2ff51ab8e6f2d6d839b970267ebe8e3c0dfb03e7
svylabs <sg@svylabs.com>
2026-02-03 10:42:57 +0100

    Updating base url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: './', // Use relative paths for GitHub Pages
+    base: './smac-viz/', // Use relative paths for GitHub Pages
     build: {
         outDir: 'dist',
         emptyOutDir: true,
